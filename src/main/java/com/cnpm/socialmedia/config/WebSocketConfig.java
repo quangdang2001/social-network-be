65a7b21fa260dcca2e43088575741ef71a84200c
quangdang2001 <quangdang2001@gmail.com>
2022-06-28 02:07:27 +0700

    fix socket

--- src/main/java/com/cnpm/socialmedia/config/WebSocketConfig.java
+++ src/main/java/com/cnpm/socialmedia/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("chat","notificationPopUp");
+        registry.enableSimpleBroker("/notification","/notificationPopUp","/chat");
         registry.setUserDestinationPrefix("/notification");
     }
 //
